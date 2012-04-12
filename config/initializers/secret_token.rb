2ee686650e25d1aa683a6bfed2a2a30d67bb49d5
bharatvegesna <bharatvegesna@yahoo.com>
2012-04-11 22:42:45 -0500

    initial MySizeCard commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySizeCard::Application.config.secret_token = '8a102cfccd695dac5deec7fb56fe87f59dc40c57b76e6a1350ac92c270acd51a13b4010400a0bc35948aac940030c803882fc1b8d1d22f89cbcdb8332c6616f0'
